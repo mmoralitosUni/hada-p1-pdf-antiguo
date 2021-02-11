200707d8914a465918ce6b4cdef59e1b98e148d3
mmorales99 <mjmorales.mcv@gmail.com>
2021-02-11 21:04:21 +0100

    Added Minutes2Seconds -- transforms minutes into seconds

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -9,7 +9,12 @@ namespace hada_p1
     class HadaP1
     {
         public static double Seconds2Minutes(double s) {
-            return ( s==0 ? 0 : ( s / 60.0 ) );
+            return ( s == 0 ? 0 : ( s / 60.0 ) );
+        }
+
+        public static double Minutes2Seconds(double m)
+        {
+            return ( m == 0 ? 0 : ( m / 60.0 ) );
         }
     }
 }
