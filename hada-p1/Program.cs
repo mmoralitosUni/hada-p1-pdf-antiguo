18fa60751ef75f4a48e61d316b420815f086a978
mmorales99 <mjmorales.mcv@gmail.com>
2021-02-11 21:21:48 +0100

    Staged basic IO Menu in Program.cs

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -8,8 +8,31 @@ namespace hada_p1
 {
     class Program
     {
+        static void printf<T>(T t) {
+            System.Console.Out.WriteLine(t);
+        }
+        static string scanf() {
+            return System.Console.In.ReadLine();
+        }
         static void Main(string[] args)
         {
+            init: printf("Que unidad desea convertir? [s=Segundos | m=Minutos | q=Salir] :  ");
+            string opt = scanf().ToLower();
+            printf("Inserte la cantidad a convertir:  ");
+            switch (opt)
+            {
+                case "s":
+                    printf(HadaP1.Seconds2Minutes(Convert.ToDouble(scanf())));
+                    break;
+                case "m":
+                    printf(HadaP1.Minutes2Seconds(Convert.ToDouble(scanf())));
+                    break;
+                case "q":
+                    goto kill;
+                default:
+                    goto init;
+            }
+        kill: return;
         }
     }
 }
